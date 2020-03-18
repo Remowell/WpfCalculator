332b06436c0fcdac63e856b8b98a24b014abddb3
Remowell <pl712993@gmail.com>
2020-03-18 21:11:44 +0500

    Добавлена кнопка удаления и ее метод (пока не работают)

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -17,7 +17,8 @@ namespace WpfCalculator
 {
     public partial class MainWindow : Window
     {
-        int indentation = 5;
+        public int indentation = 5;
+        public int count = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,15 +28,22 @@ namespace WpfCalculator
         {
             TextBox box = new TextBox();
             {
+                Name = "box" + count;
                 Margin = new Thickness(5, indentation, 5, 5);
             }
             box.TextChanged += Text_Changed;
             myStackPanel.Children.Add(box);
             indentation += 1;
-            //Button delete = new Button();
-            //{
-            //    Clic
-            //}
+
+            Button delete = new Button()
+            {
+                Content = "Delete",
+                Name = "delete" + count,
+                FontSize = 20,
+            };
+            delete.Click += Delete_Click;
+            myStackPanel.Children.Add(delete);
+            count++;
         }
         private void Text_Changed(object sender, RoutedEventArgs e)
         {
@@ -57,6 +65,24 @@ namespace WpfCalculator
                 }
             }
             lbl.Content = result;
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Button delete = (Button)sender;
+            string deleteName = delete.Name.Substring(6);
+            foreach (object element in myStackPanel.Children)
+            {
+                if (element is TextBox box)
+                {
+                    if (box.Name.Substring(3).Equals(deleteName))
+                    {
+                        myStackPanel.Children.Remove(box);
+                        myStackPanel.Children.Remove(delete);
+                        break;
+                    }
+                }
+            }
+            Text_Changed(sender, e);
         }
     }
 }
